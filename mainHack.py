d07901944ba042bc5a11f53ffcdfaf49c5edcb83
Damiano-massella <45711810+Dam-masse@users.noreply.github.com>
2019-09-28 18:21:29 +0200

    Update mainHack.py
    added the support for the inline comment

--- mainHack.py
+++ mainHack.py
@@ -13,26 +13,33 @@ def addingset(original):
     do not end with ; 
     the set command require a property and a value they are separated by , 
     if the value is a string, use! for adding the '' '''
+    if '#' in original: #splitting the comments from the commands
+        splitted=original.split('#')
+        comment=' #'+splitted[1]
+        original=splitted[0]
+    else:
+        comment=''
+
     original=original.rstrip()
-    if len(original)!=0:
-        if original[-1]==';' or original[0]=='#':
-            new=original
+    if len(original)!=0: 
+        if original[-1]==';': #check if the command is already in the lumerical format
+            new=original+comment
         else:
+
             new='set(\''
             line=original.split(',')
             setproperty=line[0].rstrip()
             new=new+setproperty+'\','
             setvalue=line[1].rstrip()
             setvalue=line[1].lstrip()
-            if setvalue[-1]=='!':
+            if setvalue[-1]=='!': # use the setvalue as a string if the ! is inserted after that
                 setvalue=setvalue.replace('!','')
                 setvalue='\''+setvalue+'\''
             new=new+setvalue
-            new=new+');'
+            new=new+');'+comment
     else:
         new=original
     return new
-
 
 
 def execution(readfile):   
@@ -43,9 +50,9 @@ def execution(readfile):
     the set command require a property and a value they are separated by , 
     if the value is a string, use! for adding the '' '''
     readfilename=readfile
-    readfile=open(readfilename)
+    readfile=open(readfilename) # open the file to read
     writefilename='write_out.txt'
-    writefile=open(writefilename,'w+')
+    writefile=open(writefilename,'w+') # open the file to write or create it
     line=readfile.readline()
     while line:
         print(line)
@@ -62,13 +69,13 @@ def execution(readfile):
     readfile.close()
     writefile.close()
     fo=open(writefilename,'r')
-    pyperclip.copy(fo.read())
+    pyperclip.copy(fo.read()) #copy the result to the clipboard
     fo.close()
     print('the result is copied in your clipboard and in the write_out.txt file')
 
     
     
-    
+execution('./pyLux/testing.txt')
     
     
     
